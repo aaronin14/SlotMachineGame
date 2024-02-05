a5acad443ef5d58e382bb5ecc85b0ddedf6fa408
Aaron Nguyen <aaron14nguyen@gmail.com>
2024-02-04 21:20:26 -0500

    Done

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
+> Aaron Hong Nguyen 
+> CS 414 - Mobile App Development
+> Homework 01
+
 # Slot Machine Game
+
+- This is a simple numeric slot machine game. When the **Play** button is clicked, it will randomly select a new set of numbers.
+- When all three numbers are the same, the user will win 5 times the amount of their bet.
+- The balance is $10 by default and it will be tracked and displayed through out the game.
+- There are a **+** button and a **-** button to adjust the bet amount.
+- The buttons are disabled and greyed out when they shouldn't be used.
+- The **Reset** button is used to reset the game state.
--- app/src/main/java/com/example/slotmachinegame/MainActivity.kt
+++ app/src/main/java/com/example/slotmachinegame/MainActivity.kt
@@ -1,11 +1,125 @@
 package com.example.slotmachinegame
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
+    private var balance : Int = 10
+    private var betAmount : Int = 1
+    private var firstNum : Int = 1
+    private var secondNum : Int = 2
+    private var thirdNum : Int = 3
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun playButton(view: View) {
+        val messageTV = findViewById<TextView>(R.id.message)
+        var message : String
+        var colorRed : Boolean
+
+        if (betAmount > balance) {
+            message = "Insufficient funds for $$betAmount bet"
+            colorRed = true
+        } else {
+            getRandomNumber()
+            if (firstNum == secondNum && firstNum == thirdNum) {
+                balance += 5 * betAmount
+                message = "Hooray! You've won"
+                colorRed = false
+            } else {
+                balance -= betAmount
+                message = "You lost! Try again"
+                colorRed = true
+            }
+        }
+        findViewById<TextView>(R.id.balance_num).text = "$$balance"
+        if (balance == 0) {
+            message = "You lost, game is over!"
+            colorRed = true
+        }
+        messageTV.text = message
+        if (colorRed) {
+            messageTV.setTextColor(Color.parseColor("#F34336"))
+        } else {
+            messageTV.setTextColor(Color.parseColor("#4CAE50"))
+        }
+        checkButtonStatus()
+    }
+    fun changeBetAmount(view: View) {
+        if (view.id == R.id.plus_button && betAmount < balance) {
+            betAmount++
+        } else if (view.id == R.id.minus_button && betAmount > 1) {
+            if (betAmount > balance) {
+                betAmount = balance
+            } else {
+                betAmount--
+            }
+        }
+        findViewById<TextView>(R.id.bet_amount_num).text = "$$betAmount"
+        checkButtonStatus()
+    }
+    fun resetButton(view: View) {
+        balance = 10
+        betAmount = 1
+        firstNum = 1
+        secondNum = 2
+        thirdNum = 3
+        displayResult()
+        findViewById<TextView>(R.id.balance_num).text = "$$balance"
+        findViewById<TextView>(R.id.bet_amount_num).text = "$$betAmount"
+        findViewById<TextView>(R.id.message).text = ""
+        checkButtonStatus()
+    }
+    private fun getRandomNumber() {
+        firstNum = generateRandomNumber()
+        secondNum = generateRandomNumber()
+        thirdNum = generateRandomNumber()
+        displayResult()
+    }
+    private fun generateRandomNumber() : Int{
+        return Random().nextInt(9)+1
+    }
+    private fun displayResult() {
+        findViewById<TextView>(R.id.first_number).text = firstNum.toString()
+        findViewById<TextView>(R.id.second_number).text = secondNum.toString()
+        findViewById<TextView>(R.id.third_number).text = thirdNum.toString()
+    }
+    private fun checkButtonStatus() {
+        val plusButton = findViewById<Button>(R.id.plus_button)
+        val minusButton = findViewById<Button>(R.id.minus_button)
+        val playButton = findViewById<Button>(R.id.play_button)
+
+        if (balance == 0) {
+            disableButton(playButton)
+            disableButton(plusButton)
+            disableButton(minusButton)
+        } else {
+            playButton.setBackgroundResource(R.drawable.blue_button)
+            playButton.isEnabled = true
+            if (betAmount >= balance) {
+                disableButton(plusButton)
+            } else {
+                plusButton.setBackgroundResource(R.drawable.green_button)
+                plusButton.isEnabled = true
+            }
+            if (betAmount == 1) {
+                disableButton(minusButton)
+            } else {
+                minusButton.setBackgroundResource(R.drawable.red_button)
+                minusButton.isEnabled = true
+            }
+        }
+    }
+    private fun disableButton(button: Button) {
+        button.setBackgroundResource(R.drawable.grey_button)
+        button.isEnabled = false
     }
 }
